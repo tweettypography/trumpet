88a6a07fd7dc49203667a50a7d656b5be21f6132
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2014-12-18 13:46:04 -0500

    Clean up some of the callback hell

--- data/user.js
+++ data/user.js
@@ -18,44 +18,52 @@ var userSchema = mongo.Schema({
 });
 
 // Cut down on the respose size by extracting only the necessary playlist properties
-function pickProperties(playlist) {
+function pickPlaylistProperties(playlist) {
 	return _.pick(playlist, 'id', 'name', 'owner', 'external_urls');
 }
 
-function populateFeed(spotifyApi, following, callback) {
-	var promises = _.map(following, function(userId) {
+// Pull a list of potential users to follow from playlists we are already subscribed to
+function usersFromPlaylists(playlists, currentUserId) {
+	return _.reduce(playlists, function(memo, playlist) {
+		if (playlist.owner.id !== 'spotify' // Trying to pull playlists owned by Spotify causes errors
+			&& playlist.owner.id !== currentUserId // We don't want our own playlists!
+			&& memo.length < 12 // Let's not follow more than 12 people on the initial seed
+			&& !_.contains(memo, playlist.owner.id)) { // Make sure the list is unique
+			memo.push(playlist.owner.id);
+		}
+		
+		return memo;
+	}, []);
+}
+
+function populateFeed(spotifyApi, user) {
+	var promises = _.map(user.following, function(userId) {
 		return spotifyApi.getUserPlaylists(userId, {limit: 42}).catch(function (err) {
 			// We're to log this error but we may want to consider dropping the user in the future
 			console.error('Failed to get playlists for:', userId, err);
 		});
 	});
 	
+	// Reset the user's feed as we are about to refill it
+	user.feed = {
+		data: []
+	};
+	
 	// Run all the promises in parallel and return them all in one big array
-	when.reduce(promises, function(memo, playlists) {
-			return memo.concat(_.map(playlists && playlists.items, pickProperties));
-		}, [])
-		.then(function (feed) {
-			callback(null, feed);
-		})
-		.catch(callback);
+	return when.reduce(promises, function(memo, playlists) {
+			memo.feed.data = memo.feed.data.concat(_.map(playlists && playlists.items, pickPlaylistProperties));
+			
+			return memo;
+		}, user);
 }
 
-function initFeed(spotifyApi, userId, callback) {
-	spotifyApi.getUserPlaylists(userId, {limit: 42})
+function initFeed(spotifyApi, userId) {	
+	return spotifyApi.getUserPlaylists(userId, {limit: 42})
 		.then(function(playlists) {
 			playlists = (playlists && playlists.items) || [];
 			
-			// Pull a list of potential users to follow from playlists we are already subscribed to
-			var following = _.reduce(playlists, function(memo, playlist) {
-				if (playlist.owner.id !== 'spotify' // Trying to pull playlists owned by Spotify causes errors
-					&& playlist.owner.id !== userId // We don't want our own playlists!
-					&& memo.length < 12 // Let's not follow more than 12 people on the initial seed
-					&& !_.contains(memo, playlist.owner.id)) { // Make sure the list is unique
-					memo.push(playlist.owner.id);
-				}
-				
-				return memo;
-			}, []);
+			// Extract the list of users
+			var following = usersFromPlaylists(playlists, userId);
 			
 			// If we actually found some people to follow we want to flag the fact that they were automatically added
 			var autoInit = following.length > 0;
@@ -63,9 +71,8 @@ function initFeed(spotifyApi, userId, callback) {
 			return userModel.findOneAndUpdate({_id: userId}, {_id: userId, following: following, autoInit: autoInit}, {upsert: true}).exec();
 		})
 		.then(function(user) {
-			populateFeed(spotifyApi, user.following, callback);
-		})
-		.catch(callback);
+			return populateFeed(spotifyApi, user);
+		});
 }
 
 userSchema.statics.getFeed = function(spotifyApi, userId, callback) {
@@ -75,36 +82,36 @@ userSchema.statics.getFeed = function(spotifyApi, userId, callback) {
 		})
 		.exec()
 		.then(function(user) {
-			// TO-DO: This section is super ugly. Clean it up
-			try {
-				if (!user || user.following.length < 1) {
-					initFeed(spotifyApi, userId, callback);
-				} else {
-					if (user.feed && user.feed.expires > Date.now()) {
-						callback(null, user.feed.data);
-					} else {
-						populateFeed(spotifyApi, user.following, function (err, data) {
-							if (!err && data) {
-								user.feed = {
-									expires: Date.now() + 600000,
-									data: data
-								};
-								
-								user.save(function(err) {
-									if (err) {
-										console.error(err);
-									}
-								});
-							}
-							
-							callback(err, data);
-						});
-					}
-				}
-			} catch (ex) {
-				callback(ex, null);
+			if (user && user.feed && user.feed.expires > Date.now() && user.feed.data && user.feed.data.length > 0) {
+				return user;
 			}
-		}, callback);
+			
+			var promise;
+			
+			if (!user || user.following.length < 1) {
+				promise = initFeed(spotifyApi, userId);
+			} else {
+				promise = populateFeed(spotifyApi, user);
+			}
+			
+			return promise
+					.then(function(user) {
+						user.feed.expires = Date.now() + 600000;
+		
+						user.save(function(err) {
+							if (err) {
+								console.error(err);
+							}
+						});
+						
+						return user;
+					});
+		})
+		.then(function(user) {
+			callback(null, user.feed);
+		}, function(err) {
+			callback(err);
+		});
 };
 
 var userModel = mongo.model('User', userSchema);
