507cf7dd90cb38cdda806a750bb3b0fdd7fee03f
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2014-12-18 00:51:34 -0500

    Add a few comments for clarity

--- data/user.js
+++ data/user.js
@@ -16,17 +16,20 @@ var userSchema = mongo.Schema({
 	autoInit: Boolean
 });
 
+// Cut down on the respose size by extracting only the necessary playlist properties
 function pickProperties(playlist) {
 	return _.pick(playlist, 'id', 'name', 'owner', 'external_urls');
 }
 
 function populateFeed(spotifyApi, following, callback) {
 	var promises = _.map(following, function(userId) {
-		return spotifyApi.getUserPlaylists(userId, {limit: 40}).catch(function (err) {
+		return spotifyApi.getUserPlaylists(userId, {limit: 42}).catch(function (err) {
+			// We're to log this error but we may want to consider dropping the user in the future
 			console.error('Failed to get playlists for:', userId, err);
 		});
 	});
 	
+	// Run all the promises in parallel and return them all in one big array
 	when.reduce(promises, function(memo, playlists) {
 			return memo.concat(_.map(playlists && playlists.items, pickProperties));
 		}, [])
@@ -37,21 +40,23 @@ function populateFeed(spotifyApi, following, callback) {
 }
 
 function initFeed(spotifyApi, userId, callback) {
-	spotifyApi.getUserPlaylists(userId, {limit: 40})
+	spotifyApi.getUserPlaylists(userId, {limit: 42})
 		.then(function(playlists) {
-			playlists = playlists.items || [];
+			playlists = (playlists && playlists.items) || [];
 			
+			// Pull a list of potential users to follow from playlists we are already subscribed to
 			var following = _.reduce(playlists, function(memo, playlist) {
-				if (playlist.owner.id !== 'spotify'
-					&& playlist.owner.id !== userId
-					&& memo.length < 10
-					&& !_.contains(memo, playlist.owner.id)) {
+				if (playlist.owner.id !== 'spotify' // Trying to pull playlists owned by Spotify causes errors
+					&& playlist.owner.id !== userId // We don't want our own playlists!
+					&& memo.length < 12 // Let's not follow more than 12 people on the initial seed
+					&& !_.contains(memo, playlist.owner.id)) { // Make sure the list is unique
 					memo.push(playlist.owner.id);
 				}
 				
 				return memo;
 			}, []);
 			
+			// If we actually found some people to follow we want to flag the fact that they were automatically added
 			var autoInit = following.length > 0;
 			
 			return userModel.findOneAndUpdate({_id: userId}, {_id: userId, following: following, autoInit: autoInit}, {upsert: true}).exec();
@@ -63,6 +68,7 @@ function initFeed(spotifyApi, userId, callback) {
 }
 
 userSchema.statics.getFeed = function(spotifyApi, userId, callback) {
+	// We save details of the user's feed in Mongo
 	userModel.findOne({
 			_id: userId
 		})
